446a45aecf88543f87ba966a2e123f4cfa30ed60
Brad Brown brad.brown@red-shift.ca <Brad Brown brad.brown@red-shift.ca>
2020-03-20 16:49:09 -0400

    Moved into Maven, added prime value checking

--- src/main/java/com/acme/App.java
+++ src/main/java/com/acme/App.java
@@ -1,11 +1,35 @@
 package com.acme;
 
-public class App 
-{
+import java.util.ArrayList;
+
+public class App {
+
     public static void main(String[] args) {
+
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+
+        primes.add(2);
+        primes.add(3);
+        primes.add(5);
+        primes.add(7);
+        primes.add(11);
+        primes.add(13);
+        primes.add(17);
+        primes.add(19);
+
+        int retVal = 0;
+
         for (int i = 0; i < args.length; i++) {
+            try {
+                int value = Integer.parseInt(args[i]);
+                if (!primes.contains(value)) {
+                    retVal = 1;
+                }
+            } catch (Exception e) {
+                retVal = 2;
+            }
             System.out.println(args[i]);
         }
-        System.exit(1);
+        System.exit(retVal);
     }
 }
