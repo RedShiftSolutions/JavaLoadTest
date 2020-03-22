93941488e63d251c991fdb6bd90fa66e8b3ad4de
Brad Brown brad.brown@red-shift.ca <Brad Brown brad.brown@red-shift.ca>
2020-03-22 00:21:43 -0400

    Used list to hold primes and initialized with literal.

--- src/main/java/com/acme/App.java
+++ src/main/java/com/acme/App.java
@@ -1,27 +1,23 @@
 package com.acme;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class App {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> primes = new ArrayList<Integer>();
-
-        primes.add(2);
-        primes.add(3);
-        primes.add(5);
-        primes.add(7);
-        primes.add(11);
-        primes.add(13);
-        primes.add(17);
-        primes.add(19);
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+                                             41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
+                                             89, 97, 101, 103, 107, 109, 113, 127, 131,
+                                             137, 139, 149, 151, 157, 163, 167, 173, 179,
+                                             181, 191, 193, 197, 199);
 
         int retVal = 0;
 
-        for (int i = 0; i < args.length; i++) {
+        for (String arg : args) {
             try {
-                int value = Integer.parseInt(args[i]);
+                int value = Integer.parseInt(arg);
                 if (primes.contains(value)) {
                     retVal = 1;
                 }
